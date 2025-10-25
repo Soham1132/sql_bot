b7cbee39eff8d45219ebebeca64badba1f3ce17e
Soham mhetre <soham.mhetre@i2econsulting.com>
2025-10-25 22:03:32 +0530

    Remove old app files from tracking

--- test_groq.py
+++
@@ -1,9 +0,0 @@
-from langchain_groq import ChatGroq
-
-llm = ChatGroq(
-    groq_api_key="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",
-    model="llama3-70b-8192"
-)
-
-response = llm.invoke("Summarize the benefits of Snowflake database.")
-print(response.content)
